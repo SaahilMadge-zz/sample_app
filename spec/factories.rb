54d5a8fd38732a3dbe7364323807893ce9de50d0
SaahilM <smadge@princeton.edu>
2013-06-12 15:53:02 -0400

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Saahil Madge"
+    email    "smadge@princeton.edu"
+    password "ohaiMark"
+    password_confirmation "ohaiMark"
+  end
+end
