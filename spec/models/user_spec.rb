af4fa0073461a5724eb1b25c221e456f80e12145
SaahilM <smadge@princeton.edu>
2013-06-17 15:47:59 -0400

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,6 +28,12 @@ describe User do
   it { should respond_to(:remember_token) }
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
 
   it { should be_valid }
 
@@ -119,9 +125,8 @@ describe User do
   end
 
   describe "micropost associations" do
-
     before { @user.save }
-    let!(:older_micropost) do 
+    let!(:older_micropost) do
       FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
     end
     let!(:newer_micropost) do
@@ -145,10 +150,21 @@ describe User do
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
       end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
 
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
     end
   end
 end
